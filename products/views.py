aee081ebbceaac1ddd7480ea3da39cd71e039832
OolongTea620 <rlafls9596@gmail.com>
2022-01-11 16:02:04 +0900

    viewcomp

--- products/views.py
+++ products/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from products.models import Menu, Category, Product
+
+class ProductsView(View):
+    def post(self, request):
+        data     = json.loads(request.body)
+        menu     = Menu.objects.create(name=data['menu'])
+        category = Category.objects.create(
+            name = data['category'],
+            menu = menu
+        )
+        Product.objects.create(
+            name     = data['product'], 
+            category = category,
+            menu     = menu
+        )
+        return JsonResponse({'messasge':'created'}, status=201)
+    
+    def get(self, request):
+        products = Product.objects.all()
+        results  = []
+
+        for product in products:
+            results.append(
+                {
+                    "menu" : product.category.menu.name,
+                    "category" : product.category.name,
+                    "product_name" : product.name
+                }
+            )
+
+        return JsonResponse({'resutls':results}, status=200)
